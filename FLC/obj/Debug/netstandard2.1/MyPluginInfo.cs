25851e736ae802c7b877a029d830ad1629d8de80
FLF3rno <gelatovariegatonutella@gmail.com>
2025-11-12 23:37:25 +0100

    change

--- FLC/obj/Debug/netstandard2.1/MyPluginInfo.cs
+++ FLC/obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -2,8 +2,8 @@ namespace FLC
 {
     public static class MyPluginInfo
     {
-        public const string PLUGIN_GUID = "FL.FLC";
+        public const string PLUGIN_GUID = "FLC";
         public const string PLUGIN_NAME = "FLC";
-        public const string PLUGIN_VERSION = "0.1";
+        public const string PLUGIN_VERSION = "0.0.1";
     }
 }
