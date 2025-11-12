4ec48bef2e0be61bab6aa160aa5a6b724451ca3e
FLF3rno <gelatovariegatonutella@gmail.com>
2025-11-12 22:34:53 +0100

    inizio

---
+++ FLC/obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace FLC
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "FL.FLC";
+        public const string PLUGIN_NAME = "FLC";
+        public const string PLUGIN_VERSION = "0.1";
+    }
+}
